fb235ce6facf22dbdb6b8efde1225da6ad8964fd
Baian Chen (Andrew) <anchen1011@gmail.com>
2018-08-02 14:40:36 -0700

    Addressed comments, tested, removed docs (reorganized into a separate PR)

--- base/lambda_function.py
+++ base/lambda_function.py
@@ -2,13 +2,23 @@ from model_service import preprocess, inference, postprocess
 
 
 class BaseResponse:
-
+    """BaseResponse if the object providing standard response attributes and operations"""
     def __init__(self, response_body):
+        """Constructor, construct a response
+        Parameters
+        ----------
+        response_body: Optionally give the response a non-empty body
+        """
         self.headers = {"content-type": "application/json", "Access-Control-Allow-Origin": "*"}
         self.statusCode = 200
         self.body = response_body
 
     def get_dict(self):
+        """Get the dictionary representation of the response
+        Return
+        ----------
+        dict: the dictionary representation of the response
+        """
         return {"headers":self.headers, "body":self.body, "statusCode":self.statusCode}
 
 
